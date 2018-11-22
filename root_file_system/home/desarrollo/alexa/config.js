cf143c89e72d52d4b5d8d31cbb059f0f094f6344
eha <eha.developer@gmail.com>
2018-11-22 10:05:29 -0600

    Alexa

--- root_file_system/home/desarrollo/alexa/config.js
+++ root_file_system/home/desarrollo/alexa/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     ip: '172.18.1.96:9200',
     taxonomias: 'taxonomias_globales',
-    negocios: 'negocios_secam',
+    negocios: 'negocios_secam'
     /*ip: '10.34.180.126:9200',
     taxonomias: 'taxonomias_globales',
     negocios: 'mex_negocios'*/
