24de3061bb578980d1fba2a3bef0b5a7e89ec697
eha <eha.developer@gmail.com>
2018-11-26 13:23:28 -0600

    API Amazon v2

--- root_file_system/home/desarrollo/alexa/config.js
+++ root_file_system/home/desarrollo/alexa/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    /*ip: '172.18.1.96:9200',
+    ip: '172.18.1.96:9200',
     taxonomias: 'taxonomias_globales',
-    negocios: 'negocios_secam'*/
-    ip: '10.34.180.126:9200',
+    negocios: 'negocios_secam'
+    /*ip: '10.34.180.126:9200',
     taxonomias: 'taxonomias_globales',
-    negocios: 'mex_negocios'
+    negocios: 'mex_negocios'*/
 }
