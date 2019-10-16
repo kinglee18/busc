c348d81344f6193a84cfee326cc355c615b863c9
king <king.lee@seccionamarilla.com>
2019-10-16 15:38:39 -0500

    fix: same city as the state

--- backend/test/unit/maps.spec.js
+++ backend/test/unit/maps.spec.js
@@ -1,5 +1,7 @@
-var assert = require('assert');
+
+var assert = require('chai').assert
 const maps = require('../../maps/address');
+const abbreviatedStates = require('../../info/syn_where');
 
 
 describe('Google maps functionality', function () {
@@ -7,14 +9,14 @@ describe('Google maps functionality', function () {
         const mexicoCityOpts = ['ciudad de mexico', 'cdmx', 'mexico', 'distrito federal', 'df'];
         for (let abbreviation of mexicoCityOpts) {
             maps.search('mexico').then(address => {
-                assert.equal(address.state, 'df', 'failed in ' + abbreviation + JSON.stringify(address));
+                assert(address.state, 'df', 'failed in ' + abbreviation + JSON.stringify(address));
             });
         }
         done();
     });
     it('should return state name depending on city in object', function (done) {
         const addresses = [
-            { input: 'coyoacan', state: 'df' , city: 'coyoacan'},
+            { input: 'coyoacan', state: 'df', city: 'coyoacan' },
             { input: 'gam', state: 'df', city: 'gustavo a. madero' },
             { input: 'neza', state: 'mex', city: 'ciudad nezahualcoyotl' },
             { input: 'oaxtepec', state: 'mor', city: 'oaxtepec' },
@@ -30,4 +32,16 @@ describe('Google maps functionality', function () {
         }
         done();
     });
+
+    it('should return an object with only the state field', (done) => {
+        for (let name of abbreviatedStates.data) {
+            maps.search(name.valor).then(result => {
+                assert.notExists(result.city, "eliminar " + result.city)
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+        done();
+    });
+
 });
