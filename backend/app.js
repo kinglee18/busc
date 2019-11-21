7a2c103e71d412133044e0afa28449b34df0582b
king <king.lee@seccionamarilla.com>
2019-11-21 10:49:30 -0600

    fix: archivo de analisis inncesario removido

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,12 @@
-const stg_clear = require('./analyzer/clear');
 const ub = require('./analyzer/wheres');
-const pln = require('./analyzer/pln');
-
 let json = {}
 
 exports.analisys = function (texto) {
-
     return new Promise((resolve, reject) => {
-
-        stg_clear.cls(texto).then((parcedText) => {
-            return pln.pln(parcedText);
-        }).then((pln) => {
-            json.pln = pln;
-            return ub.where(pln.texto);
-        }).then((resp) => {
+        ub.where(texto).then((resp) => {
             json.newSearchTerm = resp.newSearchTerm;
             json.location = resp.address;
-            json.schedule = json.pln.hrs;
-            json.price = json.pln.price;
             resolve(json);
         });
     });
 }
-
