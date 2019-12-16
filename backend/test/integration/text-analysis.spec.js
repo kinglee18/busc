4f325aaa1a67a6879820c3046f20935140d9a05a
king <king.lee@seccionamarilla.com>
2019-12-16 10:54:13 -0600

    test: text analysis

---
+++ backend/test/integration/text-analysis.spec.js
@@ -0,0 +1,69 @@
+
+var request = require('supertest');
+var assert = require('chai').assert;
+const app = require('../../index').app;
+const config = require('../../config');
+
+describe('--------------', () => {
+    before(() => {
+        process.env = Object.assign(process.env, config['test']);
+    });
+
+    const tests = [
+        { input: "hotel en DF", output: { search_term: "hotel", physicalstate: 'DISTRITO FEDERAL' } },
+        { input: "hotel en morelos", output: { search_term: "hotel", physicalstate: 'MORELOS' } },
+        { input: "hotel en oaxaca", output: { search_term: "hotel", physicalstate: 'OAXACA' } },
+        { input: "hotel en mexico", output: { search_term: "hotel", physicalstate: 'MEXICO' } },
+        { input: "hotel en san luis", output: { search_term: "hotel", physicalstate: 'SAN LUIS POTOSI' } },
+
+         {
+            input: "hotel en guadalajara", output: {
+                "physicalcity": "GUADALAJARA",
+                "physicalstate": "JALISCO",
+                "search_term": "hotel"
+            }
+        }, 
+        {
+            input: "hotel en centro de tlalpan", output: {
+                search_term: "hotel",
+                physicalstate: 'DISTRITO FEDERAL',
+                "physicalcity": "TLALPAN",
+                colony: "TLALPAN CENTRO", "postal_code": "14000"
+            }
+        },
+        {
+            input: "hotel en tlalpan centro", output: {
+                search_term: "hotel",
+                physicalstate: 'DISTRITO FEDERAL',
+                "physicalcity": "TLALPAN",
+                colony: "TLALPAN CENTRO", "postal_code": "14000"
+            }
+        },
+        {
+            input: "hotel en peña pobre", output: {
+                search_term: "hotel",
+                physicalstate: 'DISTRITO FEDERAL',
+                "physicalcity": "TLALPAN",
+                colony: "PEÑA POBRE", "postal_code": "14060"
+            }
+        }
+    ];
+    for (var i of tests) {
+        makeTest(i);
+    }
+
+    function makeTest(opc) {
+        it("It shoud fragment the input " + opc.input, function () {
+            const agent = request.agent(app);
+
+            return agent.get('/node')
+                .query({
+                    "searchTerm": opc.input,
+                    "show_business": false
+                })
+                .then(response => {
+                    assert.deepEqual(response.body.location, opc.output);
+                })
+        });
+    }
+});
