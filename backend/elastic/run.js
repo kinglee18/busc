378df28e657381c5d5af7088caecdd0e0bd7bb51
Lee Gaona, King Javan <king.lee@adn.com.mx>
2021-04-09 11:10:02 -0500

    serv

--- backend/elastic/run.js
+++ backend/elastic/run.js
@@ -46,7 +46,7 @@ function searchBusiness(page = 0, searchTerm, organicCodes, category, hrs, payme
                             constantScore('match_phrase', searchTerm, 'Appearances.Appearance.categoryname.keyword', 16, 'categoria exacta(16)'),
                             constantScore('match_phrase', searchTerm, 'bn.keyword', 20, 'nombre exacto(20)'),
                             constantScore('match_phrase', searchTerm, 'bn.spanish', 5, 'nombre parcial(5)'),
-                            constantScore('match', searchTerm, 'productservices.prdserv.keyword', 2, 'servicios(2)'),
+                            constantScore('match_phrase', searchTerm, 'productservices.prdserv', 2, 'servicios(2)'),
                             {
                                 "constant_score": {
                                     "filter": {
