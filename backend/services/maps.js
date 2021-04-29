79dcfdea72d1b1e4fbdcac29d8cb46234a60484d
Lee Gaona, King Javan <king.lee@adn.com.mx>
2021-04-29 14:52:11 -0500

    colonia con should

--- backend/services/maps.js
+++ backend/services/maps.js
@@ -27,62 +27,62 @@ exports.search = async function (address, searchTerm) {
 						stateQuery(address),
 						cityQuery(address),
 						colonyQuery(address),
-							{
-								bool: {
-									must_not:[{
-										exists: {
-											field: 'colony'
+						{
+							bool: {
+								must_not:[{
+									exists: {
+										field: 'colony'
+									}
+								}],
+								must: [
+									{
+										"multi_match": {
+											"query": address,
+											"fields": [
+												"statename",
+												"state",
+												"city",
+												"initials",
+												"statename",
+												'zc'
+											],
+											"analyzer": "spanish_analyzer",
+											"type": "cross_fields",
+											"_name": 'multi'
+										}
+									}
+								]
+							}
+						}
+					]
+				}
+				
+			}
+		}
+	} else {
+		body = {
+			"size": 1,
+			"query": {
+				"bool": {
+					"should": [
+						{
+							"bool": {
+								"must_not": [
+									{
+										"multi_match": {
+											"query": searchTerm,
+											"fields": [
+												"statename",
+												"state",
+												"city"
+											],
 										}
 									}],
-									must: [
-										{
-											"multi_match": {
-												"query": address,
-												"fields": [
-													"statename",
-													"state",
-													"city",
-													"initials",
-													"statename",
-													'zc'
-												],
-												"analyzer": "spanish_analyzer",
-												"type": "cross_fields",
-												"_name": 'multi'
-											}
-										}
-									]
-								}
-							}
-						]
-					}
-					
-				}
-			}
-		} else {
-			body = {
-				"size": 1,
-				"query": {
-					"bool": {
-						"should": [
-							{
-								"bool": {
-									"must_not": [
-										{
-											"multi_match": {
-												"query": searchTerm,
-												"fields": [
-													"statename",
-													"state",
-													"city"
-												],
-											}
-										}],
-									"must": [
+									"should": [
 										{
 											"match": {
 												"colony.spanish": searchTerm
-											}	
+											}
 										}
 									],
 									filter: [
@@ -107,193 +107,193 @@ exports.search = async function (address, searchTerm) {
 												]
 											}}
 										]
-								}
-							},
-							{
-								"multi_match": {
-									"query": searchTerm,
-									"fields": [
-										"zc",
-										"state.spanish^3",
-										"statename^3",
-										"city.spanish",
-										"statename.keyword"
-									],
-								}
-							},
-							{
-								"bool": {
-									"must": [
-										{
-											"match": {
-												"statename": searchTerm
-											}
-										},
-										{
-											"match": {
-												"city": searchTerm
-											}
-										}
-									],
-									"filter": [
-										{
-											"script": {
-												"script": {
-													"source": "if (doc['city.keyword'].size() > 0 ){doc['city.keyword'].value !=  doc['statename.keyword'].value}"
+									}
+								},
+								{
+									"multi_match": {
+										"query": searchTerm,
+										"fields": [
+											"zc",
+											"state.spanish^3",
+											"statename^3",
+											"city.spanish",
+											"statename.keyword"
+										],
+									}
+								},
+								{
+									"bool": {
+										"must": [
+											{
+												"match": {
+													"statename": searchTerm
+												}
+											},
+											{
+												"match": {
+													"city": searchTerm
 												}
 											}
-										},
-										{
-											bool: {
-												"must_not": [
-													{
-														"exists": {
-															"field": "postal_code"
-														}
-													},
-													{
-														"exists": {
-															"field": "zc"
-														}
+										],
+										"filter": [
+											{
+												"script": {
+													"script": {
+														"source": "if (doc['city.keyword'].size() > 0 ){doc['city.keyword'].value !=  doc['statename.keyword'].value}"
 													}
-												]
+												}
+											},
+											{
+												bool: {
+													"must_not": [
+														{
+															"exists": {
+																"field": "postal_code"
+															}
+														},
+														{
+															"exists": {
+																"field": "zc"
+															}
+														}
+													]
+												}
 											}
-										}
-									]
-								}
-							}
-						]
-					}
-				}
-			}
-		}
-		const requestBody = {
-			index: process.env.locations,
-			body
-		};
-		const request = await client.getClient().search(requestBody);
-		if (request.hits.hits.length) {
-		console.log(JSON.stringify(request.hits.hits[0]._id));
-
-			return {
-				"physicalstate": request.hits.hits[0]._source.statename,
-				"physicalcity": request.hits.hits[0]._source.city,
-				"postalCode": request.hits.hits[0]._source.zc || request.hits.hits[0]._source.zip,
-				"colony": request.hits.hits[0]._source.colony,
-				"initials": request.hits.hits[0]._source.initials || request.hits.hits[0]._source.state
-			};
-		}
-		return
-	}
-	
-	const stateQuery = (address) => {
-		return {
-			bool: {
-				must_not: [
-					{
-						exists: {
-							field: 'city'
-						}
-					}
-				],
-				must: [
-					{
-						"match": {
-							"state.spanish": {
-								'query': address,
-								'boost': 10,
-								'operator': 'and',
-								'_name': 'state'
-							}
-						}
-					}
-				]
-			}
-		}
-	};
-	
-	const cityQuery = (address) => {
-		return {
-			bool: {
-				must_not: [
-					{
-						"match": {
-							"state.spanish": {
-								'query': address, 
-								'operator': 'and'
-							}
-						}
-					},
-					{
-						exists: {
-							field: 'colony'
-						}
-					}
-				],
-				
-				must: [
-					{
-						"match": {
-							"city.spanish": {
-								'query': address,
-								'_name': 'city',
-								'operator': 'and',
-								'fuzziness': 1,
-								boost: 2
-							}
-						}
-					}
-				]
-			}
-		}
-	}
-
-	const colonyQuery = (address) => {
-		return {
-			"bool": {
-				must_not: [
-					{
-						"match": {
-							"state.spanish": {
-								"query": address,
-								"operator": "and"
-							}
-						}
-					},
-				],
-				must: [
-					{
-						"match": {
-							"colony.spanish": {
-								"query": address,
-								"operator": "and",
-								'_name': 'col'
-							}
-						}
-					}
-				],
-				filter: [
-					{
-						"bool": {
-							"should": [
-								{
-									"match": {
-										"statename": "DISTRITO FEDERAL"
-									}
-								},
-								{
-									"match": {
-										"city": "MONTERREY"
-									}
-								},
-								{
-									"match": {
-										"city": "GUADALAJARA"
+										]
 									}
 								}
 							]
-						}}
+						}
+					}
+				}
+			}
+			const requestBody = {
+				index: process.env.locations,
+				body
+			};
+			const request = await client.getClient().search(requestBody);
+			if (request.hits.hits.length) {
+				console.log(JSON.stringify(request.hits.hits[0]._id));
+				
+				return {
+					"physicalstate": request.hits.hits[0]._source.statename,
+					"physicalcity": request.hits.hits[0]._source.city,
+					"postalCode": request.hits.hits[0]._source.zc || request.hits.hits[0]._source.zip,
+					"colony": request.hits.hits[0]._source.colony,
+					"initials": request.hits.hits[0]._source.initials || request.hits.hits[0]._source.state
+				};
+			}
+			return
+		}
+		
+		const stateQuery = (address) => {
+			return {
+				bool: {
+					must_not: [
+						{
+							exists: {
+								field: 'city'
+							}
+						}
+					],
+					must: [
+						{
+							"match": {
+								"state.spanish": {
+									'query': address,
+									'boost': 10,
+									'operator': 'and',
+									'_name': 'state'
+								}
+							}
+						}
 					]
 				}
+			}
+		};
+		
+		const cityQuery = (address) => {
+			return {
+				bool: {
+					must_not: [
+						{
+							"match": {
+								"state.spanish": {
+									'query': address, 
+									'operator': 'and'
+								}
+							}
+						},
+						{
+							exists: {
+								field: 'colony'
+							}
+						}
+					],
+					
+					must: [
+						{
+							"match": {
+								"city.spanish": {
+									'query': address,
+									'_name': 'city',
+									'operator': 'and',
+									'fuzziness': 1,
+									boost: 2
+								}
+							}
+						}
+					]
+				}
+			}
 		}
-	} 
+		
+		const colonyQuery = (address) => {
+			return {
+				"bool": {
+					must_not: [
+						{
+							"match": {
+								"state.spanish": {
+									"query": address,
+									"operator": "and"
+								}
+							}
+						},
+					],
+					must: [
+						{
+							"match": {
+								"colony.spanish": {
+									"query": address,
+									"operator": "and",
+									'_name': 'col'
+								}
+							}
+						}
+					],
+					filter: [
+						{
+							"bool": {
+								"should": [
+									{
+										"match": {
+											"statename": "DISTRITO FEDERAL"
+										}
+									},
+									{
+										"match": {
+											"city": "MONTERREY"
+										}
+									},
+									{
+										"match": {
+											"city": "GUADALAJARA"
+										}
+									}
+								]
+							}}
+						]
+					}
+				}
+			} 
